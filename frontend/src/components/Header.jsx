4e7f68cead57dec6aa08736195d4694d4026ef94
Thuong Thuong <hoangthuong0615@gmail.com>
2025-12-26 22:26:35 +0700

    Update code

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,23 +6,35 @@ import Button from "react-bootstrap/Button";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
 function Header() {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter" && keyword.trim() !== "") {
+      e.preventDefault();
+      navigate(`/search?q=${keyword}`);
+    }
+  };
+
   return (
     <Navbar expand="lg" className="booksaw-navbar py-3">
       <Container fluid className="px-5">
-        
-        {/* LOGO – BÊN TRÁI */}
-        <Navbar.Brand href="http://localhost:5173/#" className="fw-bold fs-2">
+
+        {/* LOGO */}
+        <Navbar.Brand as={Link} to="/" className="fw-bold fs-2">
           NEVERLAND
         </Navbar.Brand>
 
         <Navbar.Toggle />
-
         <Navbar.Collapse>
 
-          {/* MENU – Ở GIỮA */}
+          {/* MENU */}
           <Nav className="mx-auto fs-5">
-            <Nav.Link href="http://localhost:5173/#">Trang chủ</Nav.Link>
+            <Nav.Link as={Link} to="/">Trang chủ</Nav.Link>
             <Nav.Link href="#">Danh mục</Nav.Link>
             <Nav.Link href="#">Sách mới</Nav.Link>
             <NavDropdown title="Thể loại">
@@ -32,12 +44,14 @@ function Header() {
             </NavDropdown>
           </Nav>
 
-          {/* SEARCH + ICON – BÊN PHẢI */}
+          {/* SEARCH */}
           <Form className="d-flex align-items-center gap-3">
             <Form.Control
               type="search"
               placeholder="Tìm sách..."
-              className="me-2"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+              onKeyDown={handleSearch}
               style={{ width: "220px" }}
             />
 
